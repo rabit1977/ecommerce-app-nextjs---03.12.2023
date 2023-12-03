11bd9a3f05753f545eede323f75cb66741e5f607
Rabit Ebibi <33910297+rabit1977@users.noreply.github.com>
2023-12-03 13:04:35 +0100

    adding navbar and footer components into the rootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import './globals.css';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/footer';
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['400', '700'] });
 
@@ -16,7 +18,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={poppins.className}>{children}</body>
+      <body className={`${poppins.className}, text-slate-700`}>
+        <div className='flex flex-col min-h-screen'>
+          <Navbar />
+          <main className='flex-grow'>{children}</main>
+          <Footer />
+        </div>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-
 export default function Home() {
-  return (
-   <main>Ecommerce app</main>
-  )
+  return <div>Ecommerce app</div>;
 }
