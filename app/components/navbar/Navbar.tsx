9949803f09537f7eaacd713736176df9df650fca
Rabit Ebibi <33910297+rabit1977@users.noreply.github.com>
2023-12-03 13:30:46 +0100

    created Navbar component

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,5 +1,34 @@
+import Link from 'next/link';
+import Container from '../Container';
+import { Redressed } from 'next/font/google';
+
+const redressed = Redressed({ subsets: ['latin'], weight: ['400'] });
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div
+      className='sticky top-0
+  w-full bg-slate-200 z-30 shadow-sm'
+    >
+      <div className='py-4 border-b'>
+        <Container>
+          <div className='flex items-center justify-between gap-3 md: md:gap-0'>
+            <Link
+              href='/'
+              className={`${redressed.className}, font-bold text-3xl`}
+            >
+              Ecommerce App
+            </Link>
+            <div className='hidden md:block'> Search</div>
+            <div className='flex items-center gap-8 md:gap-12'>
+              <div>CartCount</div>
+              <div>UserMenu</div>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
