f18d541309392279136dffa4455660bd485b90f2
Rabit Ebibi <33910297+rabit1977@users.noreply.github.com>
2023-12-03 19:16:53 +0100

    created product and cart page and made

---
+++ app/cart/page.tsx
@@ -0,0 +1,8 @@
+
+
+
+const crafted = () => {
+  return <div>Cart Page</div>;
+};
+
+export default crafted;
--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -4,17 +4,23 @@ import { formatPrice } from '@/utils/formatPrice';
 import { truncateText } from '@/utils/truncateText';
 import { Rating } from '@mui/material';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 interface ProductCardProps {
   data: any;
 }
 
 const ProductCard = ({ data }: ProductCardProps) => {
+  const router = useRouter();
+
   const productRating =
     data.reviews.reduce((acc: number, item: any) => item.rating + acc, 0) /
     data.reviews.length;
   return (
-    <div className='col-span-1 cursor-pointer border border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm'>
+    <div
+      onClick={() => router.push(`/product/${data.id}`)}
+      className='col-span-1 cursor-pointer border border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm'
+    >
       <div className='flex flex-col w-full gap-1'>
         <div className='aspect-square overflow-hidden relative w-full'>
           <Image
@@ -24,14 +30,16 @@ const ProductCard = ({ data }: ProductCardProps) => {
             alt={data.name}
           />
         </div>
-        <div className='mt-4'>{truncateText(data.name)}</div>
-        <div className='flex items-center justify-between'>
+        <div className='mt-4 text-base font-semibold'>
+          {truncateText(data.name)}
+        </div>
+        <div className='flex items-center justify-between mt-2'>
           <div>
-            <Rating value={productRating} readOnly />
+            <Rating size='small' value={productRating} readOnly />
           </div>
           <div>{data.reviews.length} reviews</div>
         </div>
-        <div className='flex justify-start font-semibold'>
+        <div className='flex justify-start text-sm font-semibold'>
           {formatPrice(data.price)}
         </div>
       </div>
