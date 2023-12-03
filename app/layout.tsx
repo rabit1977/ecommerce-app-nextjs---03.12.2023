92b683bc9144030c33071dec855a3b25812a5454
Rabit Ebibi <33910297+rabit1977@users.noreply.github.com>
2023-12-03 13:05:59 +0100

    fixed impot footer bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import './globals.css';
 import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/footer';
+import Footer from './components/footer/Footer';
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['400', '700'] });
 
